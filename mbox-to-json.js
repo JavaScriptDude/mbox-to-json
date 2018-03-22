751818e80a171b4f71e801aa62e58ee612261336
JavaScriptDude <tim.c.quinn@gmail.com>
2018-03-22 00:05:48 -0400

    Update with my own version
    Needed to make tweaks to get it working on my box. The original did not work as expected. mbox end event did not return a count.

--- mbox-to-json.js
+++ mbox-to-json.js
@@ -1,52 +1,66 @@
 #!/usr/bin/env node
+# This code is forked from https://github.com/brysonian/mbox-to-json
+# - Forked because original version did not work as-is
+# - Change made to how messageCount is calculated and handled
+# - Added allMessagesParsed() function
+# - Timeout below is to ensure that most messages are enqueued before the check occurs
 
-var MailParser  = require('mailparser').MailParser;
-var fs					= require('fs');
+var MailParser  = require('mailparser-mit').MailParser;
+var fs          = require('fs');
 var Mbox        = require('node-mbox');
-var argv				= require('yargs')
-									.alias('i', 'input')
-									.alias('o', 'output')
-									.demand(['i'])
-									.argv;
+var util        = require('util')
+var argv        = require('yargs')
+                  .alias('i', 'input')
+                  .alias('o', 'output')
+                  .demand(['i'])
+                  .argv;
 
 function main() {
-	var messages = [];
-	var total = Infinity;
-	var mbox = new Mbox();
-	var messageCount = 0;
-	mbox.on('message', function(msg) {
-	  // parse message using MailParser
-	  var mailparser = new MailParser({ streamAttachments : true });
-	  mailparser.on('end', function(mail) {
-		  messages.push(mail);
-	  	if (messages.length == messageCount) {
-	  		console.log('Finished parsing messages');
-	  		if (argv.o)
-	  			saveAsJSON(messages);
-	  		else
-	  			console.log(messages);
-	  	}
-	  });
-	  mailparser.write(msg);
-	  mailparser.end();
-	});
+  var messages = [];
+  var mbox = new Mbox();
+  var messageCount = 0;
+  mbox.on('message', function(msg) {
+    let msgCur=(messageCount++)+1
+    
+    // parse message using MailParser
+    var mailparser = new MailParser({ streamAttachments : true });
+    mailparser.on("headers", function(headers){
+        //console.log(`\n\nheader: ${headers.received}`);
+    });
+    mailparser.on('end', function(mail) {
+        messages.push(mail);
+        setTimeout(()=>{
+          // console.log(`msgCur: ${msgCur}, messageCount: ${messageCount}`)
+          if(msgCur===messageCount){// we are done
+            // console.log('Finished parsing messages');
+             allMessagesParsed(messages)
+          }
+        }, 100)
+    })
+    mailparser.write(msg);
+    mailparser.end();
+  })
 
-	mbox.on('end', function(parsedCount) {
-		console.log('Completed Parsing mbox File.');
-		messageCount = parsedCount;
-	});
+  mbox.on('end', function() {});
 
-	if (fs.existsSync(argv.input)) {
-		var handle = fs.createReadStream(argv.input);
-		//handle.setEncoding('ascii');
-		handle.pipe(mbox);
-	}
+  if (fs.existsSync(argv.input)) {
+    var handle = fs.createReadStream(argv.input);
+    handle.pipe(mbox);
+  }
 }
 
-function saveAsJSON(obj) {
-	fs.writeFile(argv.output, JSON.stringify(obj), function(err) {
-		if(err)
-			return console.log(err);
-	});
+function allMessagesParsed(messages) {
+  if (argv.o) {
+    fs.writeFile(argv.output, JSON.stringify(obj), function(err) {
+      if(err)
+        return console.log(`Failure while writing to file: ${util.inspect(err)}`);
+    });
+  } else {
+    let iMsg=1
+    messages.forEach((msg)=>{
+        console.log(`Msg ${iMsg++} of ${messages.length}: ${msg['receivedDate']} - ${msg['subject']}\n - keys Avail: ${Object.keys(msg)}\n~`)
+    })
+  }
 }
+
 main();
